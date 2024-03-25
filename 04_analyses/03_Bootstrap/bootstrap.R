d8b33329e71732e3facfcbefc70211df84e1b7a8
Valerie Milici <valeriemilici@gmail.com>
2024-03-25 10:10:04 -0700

    tiny edit

--- 04_analyses/03_Bootstrap/bootstrap.R
+++ 04_analyses/03_Bootstrap/bootstrap.R
@@ -22,6 +22,8 @@ RMF <- readRDS("04_analyses/02_models/Output/RMF.RDS")
 Mort <- readRDS("04_analyses/02_models/Output/Mortality.RDS")
 RGR <- readRDS("04_analyses/02_models/Output/RGR.RDS")
 
+add
+
 ### Bootstrap the Model --------------------------------------------------------
 ##Performance -------------------------------------------
 
